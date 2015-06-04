1540a48da73ae82f670b88aaed4b4f19ebc12c3e
kgracekennedy <kgracekennedy@gmail.com>
2015-06-04 12:17:31 -0400

    dataset created

--- Rcode/activityRR.R
+++ Rcode/activityRR.R
@@ -65,8 +65,94 @@ dayPatterns[dayPatterns$steps==max(dayPatterns$steps),]
 
 
 ###### na analysis
-sum(is.na(data))
+sum(is.na(data[[1]]))
 sum(c(T,T))
+length(is.na(data[[1]]))
 
 #check days of the week
-data[[2]]=as.date(data[[2]])
+?as.Date
+dates=as.Date(data[[2]])
+head(dates)
+head(data[[2]])
+class(dates)
+dataD=data
+data[[2]]=dates
+dayWk=weekdays(dates)
+head(dayWk)
+dataD[[4]]=as.factor(dayWk)
+naData=dataD[is.na(dataD[[1]]),]
+summary(naData)
+
+#two friday and mondays are missing entirely
+#one thursday sunday and wednesday
+dataDrmna=dataD[!is.na(dataD),]
+dataByDays=aggregate(dataDrmna$steps,by=list(day=dataDrmna$V4,dataDrmna$interval),mean)
+summary(dataByDays)
+head(dataByDays)
+class(dataD[[1]])
+newData=dataD
+class(newData[[1]])
+head(newData)
+
+head(newData$steps)
+class(newData$steps)
+class(newData$interval)
+
+#newData$steps[1]=dataByDays[dataByDays$day=="Monday" & dataByDays$Group.2=="0",3]
+head(newData)
+
+for (i in 1:length(newData[[1]])){
+  if (is.na(newData$steps[i])){
+    newData$steps[i]=
+      dataByDays[dataByDays$day==newData$V4[i] 
+                 & dataByDays$Group.2==newData$interval[i],3]
+  }
+}
+
+head(newData)
+head(dataByDays)
+dataByDays[1:10,]
+
+stepsByDayNEW=aggregate(newData$steps,by=list(Date=newData$date),sum)
+head(stepsByDayNEW)
+hist(stepsByDayNEW[[2]])
+mean(stepsByDay[[2]])
+median(stepsByDay[[2]])
+mean(stepsByDayNEW[[2]])
+median(stepsByDayNEW[[2]])
+abline(
+  v=c(mean(stepsByDay[[2]]),median(stepsByDay[[2]])),
+  col=c("red","blue")
+)
+abline(
+  v=c(mean(stepsByDayNEW[[2]]),median(stepsByDayNEW[[2]])),
+  col=c("red","blue")
+)
+#########
+head(newData)
+#Weekdays vs weekends
+weekdays=c("Monday","Tuesday","Wednesday","Thursday","Friday")
+weekend=c("Saturday","Sunday")
+#dayType=factor(c("weekday","weekend"),c(0,1))
+#class(newData[,4])
+newData[,4]=as.character(newData[,4])
+newData[,5]=character()
+k=0
+x=character()
+for (i in 1:length(newData[[1]])){
+  if (newData$V4[i] %in% weekdays){
+    newData[i,5]="weekday"
+  } else if (newData$V4[i] %in% weekend) {
+    newData[i,5]="weekend"
+  } else {
+    x[k]=newData[i,4]
+    k=k+1
+  }
+}
+head(newData)
+summary(newData$V5)
+summary(x)
+newData[,5]=as.factor(newData[,5])
+head(newData)
+names(newData)[4:5]=c("dayOfWeek","dayType")
+
